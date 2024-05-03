34b10baa0800dd64fe11f7f5b1da7a458bed72eb
unknown <chohankoti333@gmail.com>
2024-05-03 18:13:27 +0530

    03/04/24-06:15PM

--- Swagger/PostAPI.py
+++ Swagger/PostAPI.py
@@ -4,13 +4,10 @@ API = "https://fakerestapi.azurewebsites.net/api/v1/Activities"
 
 
 Payload = {
-    "firstname": "koti",
-    "lastname": "chohan",
-    "username": "koti chohan",
-    "company": "ABCD",
-    "empid": 31265,
-    "email": "chohankoti@gmail.com",
-    "password": "chohanPASSWORD@1"
+    "id": 69,
+    "title": "Activity 69",
+    "dueDate": "2024-05-03T12:16:10.1837189+00:00",
+    "completed": False
 }
 
 
@@ -18,3 +15,9 @@ response = requests.post(API,  json = Payload)
 
 print(response.status_code)
 print(response.json())
+
+data = response.json()
+print(data['id'])
+
+assert response.status_code == 200
+assert data['id'] == 69
